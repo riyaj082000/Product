b0026b7dd125dc8a7c57170e789cb0ad1a6fd12d
riyaj082000 <98595393+riyaj082000@users.noreply.github.com>
2023-03-20 14:24:06 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class HomeComponent {
 image='https://images-na.ssl-images-amazon.com/images/I/71FC6YB%2B5EL._SL1500_.jpg'
